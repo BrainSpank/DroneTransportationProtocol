80dd340cea928e8e61c48319b265649deea5067d
Nathan Ahmad <nathan.ahmad3@gmail.com>
2016-04-20 21:37:45 +0100

    Fix for root path

--- DroneKit/personal-dronekit-code/hello.py
+++ DroneKit/personal-dronekit-code/hello.py
@@ -1,6 +1,9 @@
 print "Start simulator (SITL)"
 from dronekit_sitl import SITL
-sitl = SITL(path="~/Documents/ardupilot/ArduCopter/ArduCopter.elf")
+import os
+
+
+sitl = SITL(path="/home/pi/Documents/ardupilot/ArduCopter/ArduCopter.elf")
 # sitl.download('copter', 'stable', verbose=True)
 sitl_args = ['-I0', '--model', 'quad', '--home=-35.363261,149.165230,584,353']
 sitl.launch(sitl_args, await_ready=True, restart=True)
