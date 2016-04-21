5e1965981baf203aad8fc339fc9edf619ae32a44
Nathan Ahmad <nathan.ahmad3@gmail.com>
2016-04-21 07:09:26 +0100

    Updated bluetooth connection

--- DroneKit/personal-dronekit-code/PyBluez.py
+++ DroneKit/personal-dronekit-code/PyBluez.py
@@ -11,12 +11,15 @@ def client():
     port = 3
     s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     s.connect((serverMACAddress, port))
-    while 1:
-        text = raw_input() # Note change to the old (Python 2) raw_input
-        if text == "quit":
-            break
-        s.send(text)
-    s.close()
+    #while 1:
+    #    text = raw_input() # Note change to the old (Python 2) raw_input
+    #    if text == "quit":
+    #        break
+    #    s.send(text)
+    #s.close()
+
+    return s
+
 
 def server():
 
