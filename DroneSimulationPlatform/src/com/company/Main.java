16e26fb16e1d9819b85c5c55aef9cf08364bfc88
Nathan Ahmad <nathan.ahmad3@gmail.com>
2016-05-23 10:37:37 +0100

    clean up

--- DroneSimulationPlatform/src/com/company/Main.java
+++ DroneSimulationPlatform/src/com/company/Main.java
@@ -5,58 +5,68 @@ import tests.*;
 
 public class Main {
     public static Boolean verbose = true;
-    public static Logger logger = new Logger("testLog.txt", verbose);
+    public static Logger logger = new Logger("log.txt", verbose);
 
     public static void main(String[] args) {
-/*
-        Test crashTest = new CrashTest();
-        if(crashTest.run()) {
-            Main.logger.log("Crash Test has passed!");
+
+        // CRASH TEST
+        if (true) {
+            Test crashTest = new CrashTest();
+            if (crashTest.run()) {
+                Main.logger.log("Crash Test has passed!");
+            } else {
+                Main.logger.log("CRASH TEST FAILED");
+            }
+            Main.logger.log("");
         }
-        else {
-            Main.logger.log("CRASH TEST FAILED");
-        }
-        Main.logger.log("");
 
 
-        Test successTest = new SuccessfulFlightsTest();
-        if(successTest.run()){
-            Main.logger.log("Success Test has passed!");
+        // SUCCESSFUL FLIGHT TEST
+        if(true) {
+            Test successTest = new SuccessfulFlightsTest();
+            if (successTest.run()) {
+                Main.logger.log("Success Test has passed!");
+            } else {
+                Main.logger.log("SUCCESS TEST FAILED");
+            }
+            Main.logger.log("");
         }
-        else{
-            Main.logger.log("SUCCESS TEST FAILED");
-        }
-        Main.logger.log("");
 
 
-        Test successfulSenseTest = new SuccessfulSenseTest();
-        if(successfulSenseTest.run()){
-            Main.logger.log("Successful Sense Test has passed!");
+        // SENSE TEST
+        if(true) {
+            Test successfulSenseTest = new SuccessfulSenseTest();
+            if (successfulSenseTest.run()) {
+                Main.logger.log("Successful Sense Test has passed!");
+            } else {
+                Main.logger.log("SENSE TEST FAILED");
+            }
+            Main.logger.log("");
         }
-        else{
-            Main.logger.log("SENSE TEST FAILED");
-        }
-        Main.logger.log("");
 
 
-        Test maxConcurrentBasicTest = new MaxConcurrentTest("BASICDRONE");
-        if(maxConcurrentBasicTest.run()){
-            Main.logger.log("Max Concurrent Test has passed!");
+        // BASIC DRONE MAX CONCURRENT TEST
+        if(true) {
+            Test maxConcurrentBasicTest = new MaxConcurrentTest("BASICDRONE");
+            if (maxConcurrentBasicTest.run()) {
+                Main.logger.log("Max Concurrent Test has passed!");
+            } else {
+                Main.logger.log("MAX CONCURRENT TEST FAILED");
+            }
+            Main.logger.log("");
         }
-        else{
-            Main.logger.log("MAX CONCURRENT TEST FAILED");
-        }
-        Main.logger.log("");
-*/
 
-        Test maxConcurrentRandomHeightTest = new MaxConcurrentTest("RANDOMHEIGHTDRONE");
-        if(maxConcurrentRandomHeightTest.run()){
-            Main.logger.log("Random Height Test has passed!");
+
+        // RANDOM HEIGHT DRONE TEST
+        if (true) {
+            Test maxConcurrentRandomHeightTest = new MaxConcurrentTest("RANDOMHEIGHTDRONE");
+            if (maxConcurrentRandomHeightTest.run()) {
+                Main.logger.log("Random Height Test has passed!");
+            } else {
+                Main.logger.log("RANDOM HEIGHT TEST FAILED");
+            }
+            Main.logger.log("");
         }
-        else{
-            Main.logger.log("RANDOM HEIGHT TEST FAILED");
-        }
-        Main.logger.log("");
 
 
         logger.close();
--- DroneSimulationPlatform/src/tests/CrashTest.java
+++ DroneSimulationPlatform/src/tests/CrashTest.java
@@ -11,19 +11,17 @@ public class CrashTest extends Test{
 
     public Boolean run(){
         droneData = readInDrones("crashDrones.csv");
-        // testing
-        // for drones to crash
+
         try {
             run(droneData);
         } catch (DroneCrashException e){
             // As this tests for drones crashing, catching this exception counts as a success.
-            //e.printStackTrace();
             return true;
         } finally {
             outputFlightDataInGraphableFormat("crashFlightData.m");
         }
 
-        // if test completes without catching an exception the it has failed so return false
+        // if test completes without catching an exception then it has failed so return false
         return false;
     }
 }
